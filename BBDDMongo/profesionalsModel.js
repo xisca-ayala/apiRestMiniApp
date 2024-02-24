38cdc3ceeb5ea32b2a81c468f2e59f939bd18ba0
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-02-24 23:57:54 +0100

    dia1

---
+++ BBDDMongo/profesionalsModel.js
@@ -0,0 +1,64 @@
+// const mongoose = require("mongoose");
+// const Photos = require("./profesionalsSchema");
+
+// mongoose.connect('mongodb+srv://xiscaayala:Xismaygi86!@mongonube.e1bi7ij.mongodb.net/Profesionals');
+
+ 
+
+// const createProfesionalDoc = (profData) =>{
+//     let photoDocument = new Photos(profData); 
+//     photoDocument.save()
+//     .then((data)=> {
+//         console.log(data);
+//     })
+//     .catch((err)=>{
+//         console.error(err);
+//     })
+// }
+
+// let profData1 = {
+//     name: "Lluís",
+//     age: 48, 
+//     weight: 70,
+//     height: 175,
+//     isRetired: false, 
+//     nationality: "Española",
+//     oscarNumber: 0, 
+//     profesion: "actor"
+// };
+
+// let profData2 = {
+//     name: "Carles",
+//     age: 47, 
+//     weight: 100,
+//     height: 189,
+//     isRetired: false, 
+//     nationality: "Española",
+//     oscarNumber: 0, 
+//     profesion: "actor"
+// };
+// let profData3 = {
+//     name: "Margalida",
+//     age: 41, 
+//     weight: 60,
+//     height: 162,
+//     isRetired: false, 
+//     nationality: "Española",
+//     oscarNumber: 0, 
+//     profesion: "actriz"
+// };
+// let profData4 = {
+//     name: "Carme",
+//     age: 44, 
+//     weight: 65,
+//     height: 168,
+//     isRetired: false, 
+//     nationality: "Española",
+//     oscarNumber: 0, 
+//     profesion: "actriz"
+// };
+
+// createProfesionalDoc(profData1);
+// createProfesionalDoc(profData2);
+// createProfesionalDoc(profData3);
+// createProfesionalDoc(profData4);
